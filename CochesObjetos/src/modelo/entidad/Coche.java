009e48c0ec25fe8c14d5e66e984b9452eae2c6a4
Dani Costas <daniel.costas.rodriguez@bootcamp-upgrade.com>
2024-10-21 20:17:10 +0200

    Add Coche + Motor

---
+++ CochesObjetos/src/modelo/entidad/Coche.java
@@ -0,0 +1,40 @@
+package modelo.entidad;
+
+public class Coche {
+	private String id;
+	private String marca;
+	private String modelo;
+	private Motor motor;
+
+	public Coche() {
+		super();
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public Motor getMotor() {
+		return motor;
+	}
+	public void setMotor(Motor motor) {
+		this.motor = motor;
+	}
+
+	
+		
+}
---
+++ CochesObjetos/src/modelo/entidad/Motor.java
@@ -0,0 +1,5 @@
+package modelo.entidad;
+
+public enum Motor {
+	GASOLINA, DIESEL, HIDRÓGENO
+}
